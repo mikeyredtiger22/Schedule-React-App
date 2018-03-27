d2f95640be150493a6f0f2a1d6473f35f5c59c02
Unknown <mikeyredtiger22@gmail.com>
2018-03-27 17:19:21 +0100

    Displaying API call response

--- src/App.js
+++ src/App.js
@@ -1,8 +1,33 @@
 import React, { Component } from 'react';
+import axios from "axios";
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {response: []};
+  }
+
+  componentDidMount() {
+    // this.apiCall()
+    axios.get('https://python-flask-api-schedule.herokuapp.com/')
+      .then(
+        function (response) {
+          console.log("api response data: " + response.data);
+          return JSON.stringify(response.data);
+        }
+      )
+      .then(response => this.setState({response: response}))
+      .catch(function (error) {
+        console.log("error: " + error);
+      });
+  }
+
+  // apiCall() {
+  //
+  // }
+
   render() {
     return (
       <div className="App">
@@ -13,6 +38,8 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>{this.state.response}</p>
+        {/*<button onClick={this.apiCall}>Reload</button>*/}
       </div>
     );
   }
